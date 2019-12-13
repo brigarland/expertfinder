f116c70b56984425a81054780340d44899e75ab4
Brian Garland <bgarland@bpcs.com>
2019-12-13 11:45:40 -0800

    dashboard layout

--- src/components/pages/Dashboard/Dashboard.tsx
+++ src/components/pages/Dashboard/Dashboard.tsx
@@ -11,8 +11,8 @@ export const Dashboard: React.FC = () => {
 		>
 			<div className={styles.dashLeftAside}>
 				<ContentBlock
-					icon="Search"
-					headerText="Skills, Certifications, &amp; Projects"
+					icon="ProfileSearch"
+					headerText="Find People"
 					footer={{
 						text: 'Find People',
 						link: '#',
@@ -22,24 +22,66 @@ export const Dashboard: React.FC = () => {
 				</ContentBlock>
 			</div>
 			<div className={styles.dashCentralContent}>
-				<ContentBlock
-					icon="Search"
-					headerText="Skills, Certifications, &amp; Projects"
-					footer={{
-						text: 'See All Kudos',
-						link: '#',
-					}}
-				>
-					Content
-				</ContentBlock>
+				<div className={styles.requestsCnt}>
+					<div className={styles.incomingCnt}>
+						<ContentBlock
+							icon="InboxCheck"
+							headerText="Incoming Requests"
+							footer={{
+								text: 'See All Requests',
+								link: '#',
+							}}
+						>
+							Content
+						</ContentBlock>
+					</div>
+					<div className={styles.outgoingCnt}>
+						<ContentBlock
+							icon="Send"
+							headerText="Outgoing Requests"
+							footer={{
+								text: 'See All Requests',
+								link: '#',
+							}}
+						>
+							Content
+						</ContentBlock>
+					</div>
+				</div>
+				<div className={styles.kudosCnt}>
+					<ContentBlock
+						icon="Like"
+						headerText="Kudos"
+						footer={{
+							text: 'See All Kudos',
+							link: '#',
+						}}
+					>
+						Content
+					</ContentBlock>
+				</div>
 			</div>
 			<div className={styles.dashRightAside}>
-				<ContentBlock
-					icon="Search"
-					headerText="Skills, Certifications, &amp; Projects"
-				>
-					Content
-				</ContentBlock>
+				<div className={styles.topCnt}>
+					<div className={styles.profileCnt}>
+						<ContentBlock icon="Contact" headerText="Profile">
+							Content
+						</ContentBlock>
+					</div>
+					<div className={styles.highlightCnt}>
+						<ContentBlock
+							icon="Certificate"
+							headerText="Skills, Certifications, &amp; Projects"
+						>
+							Content
+						</ContentBlock>
+					</div>
+				</div>
+				<div className={styles.metricsCnt}>
+					<ContentBlock icon="Flow" headerText="Your Connections">
+						Content
+					</ContentBlock>
+				</div>
 			</div>
 		</PageSurface>
 	)
